4bb4e580a2de92fefee7b32df6c5b28b58d50c7e
MD. Jayeen Bin Alam <jayeen6783@gmail.com>
2021-11-02 15:42:42 +0600

    jwt token setted

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ require('dotenv').config();
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+//FIREBASE INIT
+var admin = require("firebase-admin");
 
+var serviceAccount = require('./ema-john-b1bad-firebase-adminsdk-68qnu-9d59b78249.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 //middlewares
 app.use(cors());
 app.use(express.json());
@@ -12,6 +19,18 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.urbpc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+//AUTHENCATION 
+const verifyToken = async (req, res, next) => {
+    if (req.headers?.key?.startsWith('Bearer ')) {
+        const idToken = req.headers.key.split('Bearer ')[1];
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(idToken);
+            req.decodedUserEmail = decodedUser.email;
+        }
+        catch { }
+    }
+    next();
+}
 const run = async () => {
     try {
         await client.connect();
@@ -49,6 +68,20 @@ const run = async () => {
             const orderData = req.body;
             const result = await orderCollection.insertOne(orderData);
             res.json(result);
+        })
+        //GET ORDERS 
+        app.get('/orders', verifyToken, async (req, res) => {
+            const email = req.query.email;
+            if (req.decodedUserEmail === email) {
+                const query = { email: email };
+                const cursor = orderCollection.find(query).toArray();
+                const orders = await cursor;
+                res.json(orders);
+            }
+            else {
+                res.status(401).json({ message: 'user not authorized' })
+            }
+
         })
 
     }
